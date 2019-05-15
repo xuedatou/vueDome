ffd5ad50b8824669b22b5ad325970bb74a836653
xuexindong <xuexd@idss-cn.com>
2019-05-15 16:24:45 +0800

    vue官网实例

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+Vue.component('todo-item',{
+    template: '<li>这是一个代办事项</li>'
+});
 var app = new Vue({
     el: '#app',
     data: {
@@ -20,5 +23,40 @@ var app3 = new Vue({
         ]
     }
 });
+    var app5 = new Vue({
+        el: '#app5',
+        data:{
+            message: 'hellow Vue!'
+        },
+        methods:{
+            resaleMessage: function () {
+                this.message = this.message.split('').reverse().join('');
+            }
+        }
+    });
+
+    var app6 = new Vue({
+       el: '#app6',
+       data: {
+           message: "Hellow VUE!"
+       }
+    })
+Vue.component('idss-num',{
+    props: ['todo'],
+    template: '<li>{{todo.text}}还剩余：{{todo.num}}</li>'
+});
+    var app7 = new Vue({
+        el: '#app7',
+        data:{
+            list:[
+                {text: '吸尘器', num: '5'},
+                {text: '吸尘器1', num: '51'},
+                {text: '吸尘器11', num: '2'},
+                {text: '吸尘器2', num: '511'},
+            ]
+        }
+    })
+
+
 
 app.message = "I can change the data!";
